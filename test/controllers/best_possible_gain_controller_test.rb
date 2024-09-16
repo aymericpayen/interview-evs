7f7794c1627ab9af18d56d8111ccb8127b8c365f
Aymeric Payen <aymeric.payen@gmail.com>
2024-09-16 16:39:29 +0200

    uniformizing time synthax over tests using DateTime instead of plain string

--- test/controllers/best_possible_gain_controller_test.rb
+++ test/controllers/best_possible_gain_controller_test.rb
@@ -10,27 +10,27 @@ class BestPossibleGainControllerTest < ActionDispatch::IntegrationTest
     assert_equal 0, JSON.parse(response.body)['max_gain']
   end
   test 'should return max_gain = 0 when prices does not fluctuate' do
-    PotatoPrice.create!(time: '2022-08-22T09:00:00.000Z', value: 100.25)
-    PotatoPrice.create!(time: '2022-08-22T09:01:00.000Z', value: 100.25)
-    PotatoPrice.create!(time: '2022-08-22T09:02:00.000Z', value: 100.25)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.25)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 2, 0).strftime("%FT%T.%LZ"), value: 100.25)
     get best_possible_gain_show_url, params: { date: '2022-08-22' }
     p JSON.parse(response.body)
     assert_equal 0, JSON.parse(response.body)['max_gain']
   end
 
   test 'should return max_gain = 0 when prices is only decreasing during the day' do
-    PotatoPrice.create!(time: '2022-08-22T09:00:00.000Z', value: 100.25)
-    PotatoPrice.create!(time: '2022-08-22T09:01:00.000Z', value: 100.24)
-    PotatoPrice.create!(time: '2022-08-22T09:02:00.000Z', value: 100.23)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.24)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 2, 0).strftime("%FT%T.%LZ"), value: 100.23)
     get best_possible_gain_show_url, params: { date: '2022-08-22' }
     p JSON.parse(response.body)
     assert_equal 0, JSON.parse(response.body)['max_gain']
   end
 
   test 'should return max_gain = 6 when prices fluctuates positively during the day' do
-    PotatoPrice.create!(time: '2022-08-22T09:00:00.000Z', value: 100.25)
-    PotatoPrice.create!(time: '2022-08-22T09:01:00.000Z', value: 100.29)
-    PotatoPrice.create!(time: '2022-08-22T09:02:00.000Z', value: 100.31)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.29)
+    PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 2, 0).strftime("%FT%T.%LZ"), value: 100.31)
     get best_possible_gain_show_url, params: { date: '2022-08-22' }
     p JSON.parse(response.body)
     assert_equal "6.0", JSON.parse(response.body)['max_gain']
