97fd0ff88b17f3c57c09962c878e3babcce93cab
Aymeric Payen <aymeric.payen@gmail.com>
2024-09-16 15:39:34 +0200

    adds logic for BestPossibleGainController with associated tests and edge cases

--- app/services/PotatoPriceService.rb
+++ app/services/PotatoPriceService.rb
@@ -1,6 +1,7 @@
 class PotatoPriceService
   class << self
     def prices_for_date(date)
+      date = Date.parse(date)
       prices = PotatoPrice.where(time: date.beginning_of_day..date.end_of_day).order(:time)
       if prices.any?
         prices
