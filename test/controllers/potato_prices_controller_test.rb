0dfca1e5c434d7966c7d03e6eb000376ebc97507
Aymeric Payen <aymeric.payen@gmail.com>
2024-09-16 16:34:56 +0200

    extracting test related to service PotatoPriceSerivce into dedicated PotatoPriceServiceTest

--- test/controllers/potato_prices_controller_test.rb
+++ test/controllers/potato_prices_controller_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 class PotatoPricesControllerTest < ActionDispatch::IntegrationTest
-test "should get show without params and return 204" do
+test "should get show without params and return 404" do
     get potato_prices_show_url
     assert_response :not_found
   end
 
-  test "should get show with paramsa and return 200" do
+  test "should get show with params and return 404" do
     get potato_prices_show_url, params: { date: '2022-08-22' }
     assert_response :not_found
   end
@@ -21,8 +21,5 @@ test "should get show without params and return 204" do
   get potato_prices_show_url, params: { date: "2024-09-13" }
 
   assert_response :success
-  assert_equal prices.first[:value], JSON.parse(response.body).first["value"].to_d
-  assert_equal prices.first[:time], JSON.parse(response.body).first["time"]
-  assert_equal prices.length, JSON.parse(response.body).length
 end
 end
---
+++ test/services/potato_price_service_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class PotatoPriceServiceTest < ActionDispatch::IntegrationTest
+  test "should get show with params and return 404" do
+    assert_raises(ActiveRecord::RecordNotFound) do
+      PotatoPriceService.prices_for_date("2024-09-13")
+    end
+  end
+
+  test "should return prices for a given date" do
+    prices = [
+      { time: DateTime.new(2024, 9, 13, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25 },
+      { time: DateTime.new(2024, 9, 13, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.29 }
+    ]
+    PotatoPrice.create!(prices)
+
+    prices = PotatoPriceService.prices_for_date("2024-09-13")
+
+    assert_equal prices.first[:value], 100.25
+    assert_equal prices.first[:time], "Fri, 13 Sep 2024 09:00:00.000000000 UTC +00:00"
+    assert_equal prices.length, 2
+  end
+end
