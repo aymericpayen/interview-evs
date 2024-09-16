3ddcd5ecedc635b2ab0294ee2e859e828ff1b282
Aymeric Payen <aymeric.payen@gmail.com>
2024-09-16 10:57:45 +0200

    Adding logic inside consotroler to find and return prices for a given data. Also manages edge cases if no date is found or data no present in params

--- app/controllers/potato_prices_controller.rb
+++ app/controllers/potato_prices_controller.rb
@@ -1,9 +1,11 @@
 class PotatoPricesController < ApplicationController
   def show
     if price_potato_params[:date].present?
-      head :ok
+      date = Date.parse(price_potato_params[:date])
+      prices = prices_for_date(date)
+      render json: prices, status: :ok
     else
-      head :no_content
+      render json: { error: 'Please specify a date' }, status: :not_found
     end
   end
 
@@ -11,5 +13,14 @@ class PotatoPricesController < ApplicationController
 
   def price_potato_params
     params.permit(:date)
+  end
+
+  def prices_for_date(date)
+    prices = PotatoPrice.where(time: date.beginning_of_day..date.end_of_day).order(:time)
+    if prices.any?
+      prices
+    else
+      raise ActiveRecord::RecordNotFound, 'No prices found for the specified date'
+    end
   end
 end
