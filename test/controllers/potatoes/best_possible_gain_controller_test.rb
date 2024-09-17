24c964b2c0b903f858643191a468eab9da29b34d
Aymeric Payen <aymeric.payen@gmail.com>
2024-09-17 15:34:03 +0200

    correct test naming

--- test/controllers/potatoes/best_possible_gain_controller_test.rb
+++ test/controllers/potatoes/best_possible_gain_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class Potatoes::BestPossibleGainControllerTest < ActionDispatch::IntegrationTest
-  test "should return 422 when no params provided" do
+  test "returns 422 when no params provided" do
     get potatoes_best_possible_gain_path, params: { date: '08-22' }
     assert_response :unprocessable_content
     assert_equal "Please specify a valid date YYYY-mm-dd", JSON.parse(response.body)["error"]
   end
 
-    test "should return 204 when invalid params provided" do
+    test "returns 204 when invalid params provided" do
     PotatoPrice.create!(time: '2022-08-22T09:00:00.000Z', value: 100.25)
     get potatoes_best_possible_gain_path , params: { date: '2022-08-23' }
     assert_response :not_found
@@ -15,14 +15,14 @@ class Potatoes::BestPossibleGainControllerTest < ActionDispatch::IntegrationTest
   end
 
 
-  test "should return 200 and max_gain = 0 when only one price record" do
+  test "returns 200 and max_gain = 0 when only one price record" do
     PotatoPrice.create!(time: '2022-08-22T09:00:00.000Z', value: 100.25)
     get potatoes_best_possible_gain_path, params: { date: '2022-08-22' }
     assert_response :success
     assert_equal 0, JSON.parse(response.body)['max_gain']
   end
 
-  test 'should return 200 and max_gain = 6 when prices fluctuates positively during the day' do
+  test 'returns 200 and max_gain = 6 when prices fluctuates positively during the day' do
     PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25)
     PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.29)
     PotatoPrice.create!(time: DateTime.new(2022, 8, 22, 9, 2, 0).strftime("%FT%T.%LZ"), value: 100.31)
--- test/controllers/potatoes/prices_controller_test.rb
+++ test/controllers/potatoes/prices_controller_test.rb
@@ -1,24 +1,24 @@
 require "test_helper"
 
 class Potatoes::PricesControllerTest < ActionDispatch::IntegrationTest
-  test "should return 422 when no params provided" do
+  test "returns 422 when no params provided" do
     get potatoes_prices_path
     assert_response :unprocessable_content
     assert_equal "Please specify a valid date YYYY-mm-dd", JSON.parse(response.body)["error"]
   end
 
-  test "should return 422 when invalid params provided" do
+  test "returns 422 when invalid params provided" do
     get potatoes_prices_path, params: { date: '2022' }
     assert_response :unprocessable_content
     assert_equal "Please specify a valid date YYYY-mm-dd", JSON.parse(response.body)["error"]
   end
-  test "should return 204 when params provided does not match any data" do
+  test "returns 204 when params provided does not match any data" do
     get  potatoes_prices_path, params: { date: '2022-08-23' }
     assert_response :not_found
     assert_equal "No prices found for the specified date", JSON.parse(response.body)["error"]
   end
 
-  test "should return 200 when params provided and match prices" do
+  test "returns 200 when params provided and match prices" do
     prices = [
       { time: DateTime.new(2024, 9, 13, 9, 0, 0).strftime("%FT%T.%LZ"), value: 100.25 },
       { time: DateTime.new(2024, 9, 13, 9, 1, 0).strftime("%FT%T.%LZ"), value: 100.29 }
